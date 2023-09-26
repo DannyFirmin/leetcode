c78aa1bb022bf84598cc9d81336ac2aaf855a495
Danny Feng <fym8888@gmail.com>
2023-09-25 23:41:16 -0400

    Finished Q36

--- q36.cpp
+++ q36.cpp
@@ -7,6 +7,28 @@
 using namespace std;
 
 bool isValidSudoku(vector<vector<char>>& board) {
+    int rows[9][9] = {0}; // first dimension is row, second is the count
+    int columns[9][9] = {0};
+    int subBoxes[3][3][9] = {0}; // first is row, second is column, third is count
+
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            char c = board[i][j];
+            if (c != '.')
+            {
+                int index = c - '0' - 1;
+                rows[i][index]++;
+                columns[j][index]++;
+                subBoxes[i/3][j/3][index]++;
+                if (rows[i][index] > 1 || columns[j][index] > 1 || subBoxes[i/3][j/3][index] > 1)
+                {
+                    return false;
+                }
+            }
+        }
+    }
     return true;
 }
 
