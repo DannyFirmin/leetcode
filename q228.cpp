5a5542fd7b24ab8a82e41f9a709d1f60d9c7ffe9
Danny Feng <fym8888@gmail.com>
2024-01-09 20:49:02 -0500

    Submit Q228

--- q228.cpp
+++ q228.cpp
@@ -9,7 +9,22 @@ using namespace std;
 
 vector<string> findSummaryRanges(vector<int>& nums) {
     vector<string> result;
-
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int start = nums[i];
+        while (i + 1 < nums.size() && nums[i] + 1 == nums[i + 1])
+        {
+            i++;
+        }
+        if (start != nums[i])
+        {
+            result.push_back(to_string(start) + "->" + to_string(nums[i]));
+        }
+        else
+        {
+            result.push_back(to_string(start));
+        }
+    }
     return result;
 }
 
