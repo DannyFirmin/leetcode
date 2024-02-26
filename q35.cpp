3b14750f4b6edd0bbd6b227b6608656432d6b964
Danny Feng <fym8888@gmail.com>
2024-02-26 12:59:03 -0500

    Q35 AC

--- q35.cpp
+++ q35.cpp
@@ -6,11 +6,37 @@
 
 using namespace std;
 
+// my version
 int searchInsert(vector<int>& nums, int target) {
-    int left = 0;
-
-    return left;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == target) {
+            return i;
+        }
+        if (nums[i] > target) {
+            return i;
+        }
+    }
+    return nums.size();
 }
+
+// binary search
+//int searchInsert(vector<int>& nums, int target) {
+//    int pivot, left = 0, right = nums.size() - 1;
+//
+//    while (left <= right) {
+//        pivot = left + (right - left) / 2;
+//
+//        if (nums[pivot] == target) {
+//            return pivot;
+//        }
+//        if (nums[pivot] < target) {
+//            left = pivot + 1;
+//        } else {
+//            right = pivot - 1;
+//        }
+//    }
+//    return left;
+//}
 
 int main() {
     // Test cases
