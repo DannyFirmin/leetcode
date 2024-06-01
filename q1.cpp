bd69423e411e46eac1f541fff3c3a4bc001e9f35
Danny Feng <fym8888@gmail.com>
2024-05-31 23:29:33 -0400

    Q1

--- q1.cpp
+++ q1.cpp
@@ -1,3 +1,18 @@
 //
 // Created by Danny Feng on 5/30/24.
 //
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for (int i = 0; i < nums.size(); i++) {
+            int valueToCheck = target - nums[i];
+            if (hashmap.find(valueToCheck) != hashmap.end()) {
+                return {hashmap[valueToCheck], i};
+            }
+            hashmap[nums[i]] = i;
+        }
+        return {};
+    }
+
+};
