1c75d617333e96542ebfe6113c079a4e21658930
Danny Feng <fym8888@gmail.com>
2024-07-16 23:27:51 -0400

    Q1047

---
+++ q1047.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Danny Feng on 7/16/24.
+//
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        string result;
+        for(char s : S) {
+            if(result.empty() || result.back() != s) {
+                result.push_back(s);
+            }
+            else {
+                result.pop_back();
+            }
+        }
+        return result;
+    }
+};
