e545df7eed37ece51008e5c40b6aabf7b00f6a0f
Danny Feng <fym8888@gmail.com>
2024-09-17 23:51:40 -0400

    Q538

---
+++ q538.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Danny Feng on 9/17/24.
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int pre = 0;  // previous node value
+    void traversal(TreeNode* cur) { // right middle left traversal
+        if (cur == nullptr) return;
+        traversal(cur->right);
+        cur->val += pre;
+        pre = cur->val;
+        traversal(cur->left);
+    }
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        pre = 0;
+        traversal(root);
+        return root;
+    }
+};
