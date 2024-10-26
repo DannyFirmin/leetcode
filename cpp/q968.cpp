83db945c99da32413617cab89d072b1392ac576a
Danny Feng <fym8888@gmail.com>
2024-10-26 02:46:05 -0400

    Q968

---
+++ cpp/q968.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Danny Feng on 10/25/24.
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int result;
+    int traversal(TreeNode* cur) {
+
+        if (cur == NULL) return 2;
+
+        int left = traversal(cur->left);
+        int right = traversal(cur->right);
+
+        if (left == 2 && right == 2) return 0;
+
+        if (left == 0 || right == 0) {
+            result++;
+            return 1;
+        }
+
+        if (left == 1 || right == 1) return 2;
+
+        return -1;
+    }
+
+public:
+    int minCameraCover(TreeNode* root) {
+        result = 0;
+        if (traversal(root) == 0) {
+            result++;
+        }
+        return result;
+    }
+};
