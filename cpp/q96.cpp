5636a56509b6e187afb70ee7e3346535babed400
Danny Feng <fym8888@gmail.com>
2024-10-30 23:26:54 -0400

    Q96

---
+++ cpp/q96.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Danny Feng on 10/30/24.
+//
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
