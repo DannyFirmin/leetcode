fffe9b58288239f1af10cca190ade4793fc8baf4
Danny Feng <fym8888@gmail.com>
2024-11-11 20:24:06 -0500

    Q121

---
+++ cpp/q121.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Danny Feng on 11/11/24.
+//
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int low = INT_MAX;
+        int result = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            low = min(low, prices[i]);
+            result = max(result, prices[i] - low);
+        }
+        return result;
+    }
+};
