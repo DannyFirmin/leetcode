677f0558eff0c09b53c7a23bb6e48d6e843fbc1e
Danny Feng <fym8888@gmail.com>
2025-02-15 00:27:49 -0500

    Q242 AC

--- java/src/q242.java
+++ java/src/q242.java
@@ -1,5 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class q242 {
     public boolean isAnagram(String s, String t) {
-        return false;
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> charCount = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            Integer count = charCount.get(s.charAt(i));
+            if (count == null) {
+                charCount.put(s.charAt(i), 1);
+            } else {
+                charCount.put(s.charAt(i), ++count);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            Integer count = charCount.get(t.charAt(i));
+            if (count == null) {
+                return false;
+            } else {
+                charCount.put(t.charAt(i), --count);
+            }
+        }
+
+        for (Integer count : charCount.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
