75779c7088e47acda0be97aa62cdb69d34b1b691
Danny Feng <fym8888@gmail.com>
2025-04-13 00:46:06 -0400

    Q143

---
+++ java/src/q143.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class q143 {
+    public void reorderList(ListNode head) {
+        // find middle
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // reverse second half
+        ListNode second = slow.next;
+        ListNode prev = slow.next = null;
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+        // merge two lists
+        second = prev;
+        ListNode first = head;
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
