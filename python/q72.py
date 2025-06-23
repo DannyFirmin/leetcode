6ea3a417df578470c782da813fbca299b73a73ee
Danny Feng <fym8888@gmail.com>
2025-06-23 01:33:32 -0400

    Fix q72

--- python/q72.py
+++ python/q72.py
@@ -2,7 +2,7 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
 
-        for j in range(len(w ßord2) + 1):
+        for j in range(len(word2) + 1):
             dp[len(word1)][j] = len(word2) - j
         for i in range(len(word1) + 1):
             dp[i][len(word2)] = len(word1) - i
